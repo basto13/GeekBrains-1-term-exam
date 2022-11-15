ba1c83364c2378b266b26ccd011e15b191d8d517
Olga Lushnikova <63054910+basto13@users.noreply.github.com>
2022-11-15 18:55:15 +0200

    add structure of function to form the new list

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Из имеющегося массива сформировать массив из строк, 
+// длина которых меньше либо равна 3 символам.
+
+Console.Write("The list of items are: ['hello', '2', 'world', ':-)']");
+
+void newList(string [] list)
+{
+    string [] newList = new string []{""};
+
+
+    Console.WriteLine(newList);
+}
+
+string [] list = new string []{"hello", "2", "world", ":-)"};
