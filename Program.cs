3d18201f96bec5f5680e163989e6773ff2af508d
Olga Lushnikova <63054910+basto13@users.noreply.github.com>
2022-11-15 19:29:30 +0200

    finish newList function

--- Program.cs
+++ Program.cs
@@ -3,12 +3,31 @@
 
 Console.Write("The list of items are: ['hello', '2', 'world', ':-)']");
 
-void newList(string [] list)
+string[] list = new string[] { "hello", "2", "world", ":-)" };
+string[] newList = new string[list.Length];
+
+void formNewList(string[] list, string[] newList)
 {
-    string [] newList = new string []{""};
-
-
-    Console.WriteLine(newList);
+    int count = 0;
+    for (int i = 0; i < list.Length; i++)
+    {
+        if (list[i].Length < 4)
+        {
+            newList[count] = list[i];
+            count++;
+        }
+    }
 }
 
-string [] list = new string []{"hello", "2", "world", ":-)"};
+void printList(string[] newList)
+{
+    Console.WriteLine();
+    Console.WriteLine("New list is ");
+    for (int i = 0; i < newList.Length; i++)
+    {
+        Console.WriteLine($"{newList[i]} ");
+    }
+}
+
+formNewList(list, newList);
+printList(newList);
